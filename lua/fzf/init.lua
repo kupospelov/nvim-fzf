e6f179f46df2016d8af07099d3000ea413b36ec6
Konstantin Pospelov <kupospelov@gmail.com>
2024-01-20 11:03:28 +0100

    Use vim.list_extend to merge options

--- lua/fzf/init.lua
+++ lua/fzf/init.lua
@@ -6,17 +6,9 @@ local config = {
 	},
 }
 
-local merge_table = function(t1, t2)
-	if not t2 then return t1 end
-	for i = 1, #t2 do
-		t1[#t1 + 1] = t2[i]
-	end
-	return t1
-end
-
 local merge_options = function(options)
 	return {
-		options = merge_table(options, config.options),
+		options = vim.list_extend(options, config.options),
 		layout = config.layout,
 	}
 end
@@ -35,8 +27,7 @@ local M = {}
 
 M.setup = function(c)
 	assert(c, 'no config overrides provided')
-	config.layout = c.layout or config.layout
-	config.options = c.options or config.options
+	config = vim.tbl_extend('force', config, c)
 end
 
 M.buffers = function()
